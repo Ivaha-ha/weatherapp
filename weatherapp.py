032258d9e3a04ab15b705f0f5fdc060c30b24f05
Ivaha <rentrooms.lviv.ua@gmail.com>
2021-03-25 06:38:44 +0200

    weatherapp temp and condition

--- weatherapp.py
+++ weatherapp.py
@@ -5,45 +5,79 @@ import html
 from urllib.request import urlopen, Request
 
 ACCU_URL = 'https://www.accuweather.com/uk/ua/lviv/324561/weather-forecast/324561'
+ACCU_TAGS = ('<div class="temp">', '<span class="phrase">')
 
-#getting url from server
-
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
-accu_request = Request (ACCU_URL, headers=headers)
-accu_page = urlopen (accu_request).read()
-accu_page = str (accu_page)
-
-accu_temp_tag = '<div class="temp">'
-accu_temp_tag_size = len (accu_temp_tag)
-accu_temp_tag_index = accu_page.find (accu_temp_tag)
-accu_temp_value_start = accu_temp_tag_index + accu_temp_tag_size
-accu_temp = ''
-for char in accu_page [accu_temp_value_start:]:
-	if char != '<': 
-		accu_temp += char
-	else:
-		break
-
+#accu_temp_tag = '<div class="temp">'
 #accu_phrase_tag = '<span class="phrase">'	 
-#accu_phrase_tag_size = len (accu_phrase_tag)
-#accu_phrase_tag_index = accu_page.find (accu_phrase_tag)
-#accu_phrase_value_start = accu_phrase_tag_index + accu_phrase_tag_size
-#accu_phrase = ''
-#for char_phrase in accu_page [accu_phrase_value_start:]:
-#	if char_phrase != '<':
-#		accu_phrase += char_phrase
-#	else:
-#		break 
+
+def get_request_headers():
+	return {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64)'}
+
+
+def get_page_source(url):
+	"""
+	дістаємо дані з сторінки і повертає як стрічку, а не як байти
+	"""
+	request = Request (url, headers = get_request_headers())
+	page_source = urlopen(request).read()
+	return page_source.decode('utf-8')
+
+def get_tag_content(page_content, tag):
+	"""
+	"""
+	tag_index = page_content.find(tag)
+	tag_size = len (tag)
+	value_start = tag_index + tag_size
+	
+	content = ''
+	for c in page_content[value_start:]:
+		if c != '<':
+			content += c
+		else:
+			break
+	return content
+		
+def get_weather_info (page_content, tags):
+	return tuple ([get_tag_content (page_content, tag) for tag in tags])
+
+def produce_output(provider_name, temp, condition):
+	"""
+	"""
+	print (f'\n{provider_name}:')
+	print (f'TEMPERATURE: {html.unescape(temp)} \n')
+	print (f'CONDITION: {html.unescape(condition)} \n')
+	
+
+def main():          #!!!!!!!!!!!!!!!!!!!!!! 
+	""" main entry point
+	"""
+	weather_sites = {"ACCUWEAHER": (ACCU_URL, ACCU_TAGS)} #"RP5": (PR5_URL, RP5_TAGS)
+	# weather_sites - словник
+	for name in weather_sites: 
+		url, tags = weather_sites[name]  # кортеж 
+		content = get_page_source(url)
+		temp, condition = get_weather_info (content, tags)
+		produce_output (name, temp, condition)
+	                 
+
+	
+if __name__ == '__main__':
+	main()
 
 
 
-print ('ACCUWEATHER: \n')
-print (f'TEMPERATURE: {html.unescape(accu_temp)} \n')
-#print (f'PHRASE: {html.unescape(accu_phrase)} \n')
+# python3 weatherapp.py
 
 
 
-#user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36
+
+
+
+
+
+
+
+
 
 
 
